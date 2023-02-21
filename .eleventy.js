b842adc77b65cbbfc9d664f4599f8f4cc675f88c
webtactics <paul@webtactics.com.au>
2023-02-21 14:44:32 +1100

    230221

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,14 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
 
 eleventyConfig.addPassthroughCopy('./src/style.css');
-eleventyConfig.addPassthroughCopy('./src/assets')
+eleventyConfig.addPassthroughCopy('./src/assets');
+eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
+eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
     return {
     dir: {
         input: "src",
