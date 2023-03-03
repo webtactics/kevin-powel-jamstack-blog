ecb52f7754539c905d6a978fad9cb18740d77134
webtactics <paul@webtactics.com.au>
2023-03-03 16:31:15 +1100

    230303-more

--- .eleventy.js
+++ .eleventy.js
@@ -7,8 +7,14 @@ eleventyConfig.addPassthroughCopy('./src/assets');
 eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
 eleventyConfig.addFilter("postDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED); 
   });
+
+    eleventyConfig.addFilter("limit", function (arr, limit) {
+      return arr.slice(0, limit);
+    });
+
+ 
     return {
     dir: {
         input: "src",
