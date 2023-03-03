340c65c161d188a8ccecd3f607dc97cebc0c69cc
webtactics <paul@webtactics.com.au>
2023-03-03 17:32:54 +1100

    fix config

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
 
 eleventyConfig.addPassthroughCopy('./src/style.css');
 eleventyConfig.addPassthroughCopy('./src/assets');
+eleventyConfig.addPassthroughCopy('./src/admin');
 eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
 eleventyConfig.addFilter("postDate", (dateObj) => {
